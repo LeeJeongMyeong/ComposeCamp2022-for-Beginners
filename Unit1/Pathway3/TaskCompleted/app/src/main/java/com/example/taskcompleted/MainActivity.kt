32309a6bb699f63ec08286d390ba5cf079b04e71
jm.lee <rhatut@gmail.com>
2022-11-10 19:57:35 +0900

    - TaskCompleted : 완

--- Unit1/Pathway3/TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
+++ Unit1/Pathway3/TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
@@ -9,11 +9,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,15 +21,54 @@ import com.example.taskcompleted.ui.theme.TaskCompletedTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            Surface(color = MaterialTheme.colors.background) {
+                TaskCompletedTheme(false) {
+                    TaskCompletedScreen()
+                }
+            }
+        }
     }
 }
 
 @Composable
 fun TaskCompletedScreen() {
-    Column( ) { }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_task_completed),
+            contentDescription = null,
+            modifier = Modifier
+                .wrapContentWidth(CenterHorizontally)
+        )
+
+        Text(
+            text = stringResource(id = R.string.all_task_completed),
+            fontSize = 24.sp,
+            modifier = Modifier
+                .wrapContentWidth(CenterHorizontally)
+                .padding(top = 24.dp, bottom = 8.dp)
+        )
+
+        Text(
+            text = stringResource(id = R.string.nice_work),
+            fontSize = 16.sp,
+            modifier = Modifier
+                .wrapContentWidth(CenterHorizontally)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+    TaskCompletedTheme(false) {
+        TaskCompletedScreen()
+    }
+}
